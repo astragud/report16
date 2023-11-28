b3db3f70e3876b9d4c7d37aaeda4b53d2102a54b
alsgud <127361355+astragud@users.noreply.github.com>
2023-11-28 10:58:25 +0900

    report16.cpp

--- report16/report16.cpp
+++ report16/report16.cpp
@@ -54,7 +54,7 @@ static void merge(int list[], int left, int mid, int right)
     swaps = swaps + moves;
 }
 
-<<<<<<< HEAD
+
 static void merge_sort(int list[], int size, int k)
 {
     int left, mid, right;
@@ -80,55 +80,13 @@ static void merge_sort(int list[], int size, int k)
             }
         }
         step *= 2;
-=======
-
-
-static void merge_sort(int list[], int left, int right, int k)
-{
-    int mid;
-    if (left < right)
-    {
-        mid = (left + right) / 2; // 리스트의 균등분할
-        merge_sort(list, left, mid, k); // 부분리스트 정렬
-        merge_sort(list, mid + 1, right, k);//부분리스트 정렬
-        
-        if (k == 0)
-        {
-            for (int i = 0; i < MAX_SIZE; i++)
-            {
-                printf("%d ", list[i]);
-            }
-            printf("\n");
-        }
-
-        merge(list, left, mid, right); // 합병
->>>>>>> 8712b8638d0305098f7490a513ceef41ba82e78b
     }
 }
 
 int main()
 {
-<<<<<<< HEAD
-    for (int j = 0; j < 20; j++)
-    {
-        int list[MAX_SIZE]{};
-        srand(time(NULL));
-
-        if (j == 0)
-        {
-            // 난수 생성 및 초기 배열 출력
-            printf("초기 배열\n");
-            for (int i = 0; i < MAX_SIZE; i++)
-            {
-                list[i] = rand() % 100;
-                printf("%d ", list[i]);
-            }
-            printf("\n\n");
-=======
-    com = 0, swaps = 0;
     int list[MAX_SIZE]{};
     srand(time(NULL));
-
     for (int j = 0; j < 20; j++)
     {
         for (int i = 0; i < MAX_SIZE; i++)
@@ -137,36 +95,21 @@ int main()
         }
 
         if (j == 0)
-        {// 초기 배열 출력
+        {
+            // 초기 배열 출력
             printf("초기 배열\n");
             for (int i = 0; i < MAX_SIZE; i++)
             {
                 printf("%d ", list[i]);
             }
             printf("\n\n");
-
->>>>>>> 8712b8638d0305098f7490a513ceef41ba82e78b
         }
-
+      
         // 합병 정렬 수행
-        if (j == 0)
-        {
-            printf("Merge Sort\n");
-        }
-<<<<<<< HEAD
         merge_sort(list, MAX_SIZE, j);
     }
-    
-    // 비교 횟수와 이동 횟수 출력
-    printf("Average Compare Count: %d\n", com / 20);
-    printf("Average Move Count: %d\n", swaps / 20);
-=======
-        
-        merge_sort(list, 0, MAX_SIZE - 1, j);
-    }
-    
+
     printf("average compare count  : %d\n", com / 20);
     printf("average move count  : %d\n", swaps / 20);
->>>>>>> 8712b8638d0305098f7490a513ceef41ba82e78b
     return 0;
 }
